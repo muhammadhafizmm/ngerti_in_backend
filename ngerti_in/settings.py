62d948950f155529754bd8c7684c4239bee64363
muhammadhafizmm <m.hafizmaulana777@gmail.com>
2021-12-08 13:04:32 +0700

    deploy heroku

--- ngerti_in/settings.py
+++ ngerti_in/settings.py
@@ -135,13 +135,13 @@ USE_TZ = True
 # Static files(CSS, JavaScript, Images)
 # https: //docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories
 # for collectstatic to find static files
 STATICFILES_DIRS = (
-   os.path.join(PROJECT_ROOT, 'static'),
+   os.path.join(PROJECT_ROOT.replace("settings.py", ""), 'static'),
 )
 
 
