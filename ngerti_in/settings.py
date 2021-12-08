258a689acf48ad5b86576890554c716579dab891
muhammadhafizmm <m.hafizmaulana777@gmail.com>
2021-12-08 12:40:01 +0700

    deploy heroku

--- ngerti_in/settings.py
+++ ngerti_in/settings.py
@@ -136,9 +136,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
