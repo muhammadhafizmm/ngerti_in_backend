abea0112891162930975f85ec4791e27d34f478a
nadhira.rafik <nadhira.rafik19@gmail.com>
2021-12-09 08:09:28 +0700

    json cancel

--- tryout/views.py
+++ tryout/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.generics import get_object_or_404
 
-import csv, json, time
+import csv, time
 
 from .serializers import (
     TryOutSerializer
@@ -26,22 +26,15 @@ class TryOutViewSet(viewsets.ModelViewSet):
         all_data = serializer.data
         soal = all_data["file_soal"]
         csvFilePath = soal[1:4] + ".csv"
-        jsonFilePath = "to_file.json"
         data = []
         with open(csvFilePath) as csvFile:
             csvReader = csv.DictReader(csvFile)
             for csvRow in csvReader:
                 data.append(csvRow)
 
-        with open(jsonFilePath, "w") as jsonFile:
-            jsonFile.write(json.dumps(data, indent=4))
+        response = Response()
+        response.data = {"tryout_detail" : serializer.data,
+        "soal" : data}
 
-        durasi = all_data["durasi_pengerjaan"]
-        durasi_detik = durasi * 60
-        while durasi_detik :
-            mins, secs = divmod(durasi_detik, 60)
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(timer, end="\r")
-            time.sleep(1)
-            durasi_detik -= 1
-        return Response(serializer.data)
+        return response
+
